9341ceb2e039369e9b1d3a8bec97e9a5e87c7a87
tasnimul.hasan <mdtasnimulh@gmail.com>
2022-10-29 16:17:10 +0600

    Bdjobs Design

--- app/src/main/java/com/example/bdjobsdesign/MainActivity.kt
+++ app/src/main/java/com/example/bdjobsdesign/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.bdjobsdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonChange = findViewById<Button>(R.id.requestChangedButton)
+        buttonChange.setOnClickListener {
+            val intent = Intent(this@MainActivity, NextDesign::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/bdjobsdesign/NextDesign.kt
@@ -0,0 +1,11 @@
+package com.example.bdjobsdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextDesign : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_design)
+    }
+}
