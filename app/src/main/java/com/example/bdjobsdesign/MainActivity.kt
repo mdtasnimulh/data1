cf27e6a6857fde67c1480b97d0709423948f86a5
tasnimul.hasan <mdtasnimulh@gmail.com>
2022-10-30 13:39:15 +0600

    Bdjobs Design

--- app/src/main/java/com/example/bdjobsdesign/MainActivity.kt
+++ app/src/main/java/com/example/bdjobsdesign/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.bdjobsdesign
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +24,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, NextDesign::class.java)
             startActivity(intent)
         }
+
+        textView = findViewById(R.id.timeLeftText)
+        val text: String = "Time Left for Online Test for Design: 5 Days"
+        val spannableString = SpannableString(text)
+        val styleSpan = StyleSpan(Typeface.BOLD)
+        spannableString.setSpan(styleSpan, 14, 44, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.setText(spannableString).toString()
     }
 }
--- app/src/main/java/com/example/bdjobsdesign/NextDesign.kt
+++ app/src/main/java/com/example/bdjobsdesign/NextDesign.kt
@@ -1,11 +1,28 @@
 package com.example.bdjobsdesign
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.widget.TextView
 
 class NextDesign : AppCompatActivity() {
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next_design)
+
+        textView = findViewById(R.id.timeLeftText)
+        val text: String = "Time Left for Online Test for Design: 00:03:17"
+        val spannableString = SpannableString(text)
+        val colorSpan = ForegroundColorSpan(Color.RED)
+        val styleSpan = StyleSpan(Typeface.BOLD)
+        spannableString.setSpan(colorSpan,38, 46, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(styleSpan, 14, 46, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.setText(spannableString).toString()
     }
 }
